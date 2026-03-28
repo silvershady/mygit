1f36ca1fc5e994015ada77849db669c0e161a1a2
titus <titusjenowin123@gmail.com>
2026-03-28 22:09:34 +0530

    added macros in c++

---
+++ c++/macro.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(int argc, char *argv[])
+ {
+ for(int i = 1; i < argc; ++i)
+ std::cout << argv[i] << std::endl;
+ std::cout << "invoked with " << argv[0] << std::endl;
+ }
