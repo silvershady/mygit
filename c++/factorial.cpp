eb6c78b178945d9878592d5bcac372a402ac2f4b
titus <titusjenowin123@gmail.com>
2026-03-28 22:06:55 +0530

    add factorial in c++

---
+++ c++/factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter a number for factorial: ";
+    int n, fact = 1;
+    cin >> n;
+
+    if (n < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        cout << "Factorial of " << n << " is " << fact << endl;
+    }
+
+    return 0;
+}
